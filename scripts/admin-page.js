2ec4b66e6913edebdaa1d36e8fe0861d6291bab9
jrwessen <you@example.com>
2024-06-05 12:54:02 +0200

    Modified .gitignore and update package.json to allow cors policy

--- scripts/admin-page.js
+++ scripts/admin-page.js
@@ -1,19 +1,25 @@
 //.............SIDE BAR FUNCTIONS ...................
 
-document.querySelectorAll(".sidebar p").forEach(function (p) {
-  p.addEventListener("click", function () {
-    // Remove the 'clicked' class from all <p> tags
-    document.querySelectorAll(".sidebar p").forEach(function (p) {
-      p.classList.remove("clicked");
-    });
-    // Add the 'clicked' class to the clicked <p> tag
-    p.classList.add("clicked");
-  });
-});
-
 //.............PIN FUNCTIONS ...................
 document
   .getElementById("generate-pin-btn")
   .addEventListener("click", function () {
     alert("Button clicked!");
   });
+
+//------------------------------API FUNCTIONS -------------------------------
+
+fetch("http://localhost:3000/api/storyAPI")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("There was a problem with the fetch operation:", error);
+  });
+//--------------------------------------------------------------------------
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const routes = require("./routes/routes");
+
+app.use(cors());
 
 app.use(express.json());
 
 app.use("/api/storyAPI", routes);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
