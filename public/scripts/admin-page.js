01b840143fa4042195753a8b45b33b21a4a756f7
jrwessen <you@example.com>
2024-06-05 15:58:26 +0200

    Added socket.io

--- public/scripts/admin-page.js
+++ public/scripts/admin-page.js
@@ -14,6 +14,8 @@ function generatePin() {
   let formattedPin = pinString.slice(0, 3) + " " + pinString.slice(3);
   document.querySelector("#pin-code").innerText = formattedPin;
 }
+
+//=============================================================================================================
 //------------------------------API FUNCTIONS -------------------------------
 
 fetch("http://localhost:3000/api/storyAPI")
@@ -29,4 +31,43 @@ fetch("http://localhost:3000/api/storyAPI")
   .catch((error) => {
     console.error("There was a problem with the fetch operation:", error);
   });
-//--------------------------------------------------------------------------
+//--------------------------SOCKET --------------------------------
+
+const socket = io();
+
+// Emit a joinSession event upon connection
+socket.emit("joinSession", "session123");
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  // Set up the click event listener after the DOM is fully loaded
+  document
+    .querySelector("#show-on-big-screen-btn")
+    .addEventListener("click", functionthing);
+
+  // Set up other event listeners if needed
+  document.getElementById("start-btn").onclick = () => {
+    socket.emit("adminAction", {
+      sessionId: "session123",
+      action: "startGame",
+    });
+  };
+});
+
+// Function to handle button click and emit adminAction event
+const functionthing = () => {
+  socket.emit("adminAction", {
+    sessionId: "session123",
+    action: "showPin",
+  });
+};
+
+// Listen for adminAction events from the server
+socket.on("adminAction", (action) => {
+  if (action === "showPin") {
+    console.log("Show Pin Code here");
+  } else if (action === "startGame") {
+    console.log("Show start game");
+  }
+});
+
+//=============================================================================================================
