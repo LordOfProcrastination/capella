521893212efe97866415df26ed9dce1296f4b52e
jrwessen <you@example.com>
2024-06-06 23:56:42 +0200

    Updated admin-page.js

--- public/scripts/admin-page.js
+++ public/scripts/admin-page.js
@@ -1,23 +1,38 @@
 const socket = io();
+
 let formattedPin = "111 111";
+
 //.............SIDE BAR FUNCTIONS ...................
 
-//............. EVENT LISTENER (BUTTONS)  ...................
+//............. EVENT LISTENER (BUTTONS)  ...................
+
 document.addEventListener("DOMContentLoaded", (event) => {
   document
+
     .getElementById("generate-pin-btn")
+
     .addEventListener("click", confirmPinGeneration);
+
   document
+
     .getElementById("play-scene-btn")
+
     .addEventListener("click", playScene);
+
   document
+
     .getElementById("pause-scene-btn")
+
     .addEventListener("click", pauseScene);
+
   document.getElementById("ask-btn").addEventListener("click", questionStart);
 });
+
 //.............PIN FUNCTIONS ...................
+
 function confirmPinGeneration() {
   const userConfirmed = confirm("Are you sure you want to generate a new PIN?");
+
   if (userConfirmed) {
     generatePin();
   }
@@ -25,17 +40,24 @@ function confirmPinGeneration() {
 
 function generatePin() {
   let pin = Math.floor(100000 + Math.random() * 900000);
+
   let pinString = pin.toString();
+
   formattedPin = pinString.slice(0, 3) + " " + pinString.slice(3);
+
   document.querySelector("#pin-code").innerText = formattedPin;
+
   socket.emit("generatePin", formattedPin);
 }
 
 // Function to handle button click and emit adminAction event
+
 const showPin = () => {
   socket.emit("generatePin", formattedPin);
 };
+
 //.............FUNCTIONS ...................
+
 function updateEventMessage(message) {
   document.getElementById("event-message").textContent = message;
 }
@@ -43,28 +65,66 @@ function updateEventMessage(message) {
 function playScene() {
   socket.emit("adminAction", {
     sessionId: "session123",
+
     action: "playScene",
   });
 }
 
 function pauseScene() {
   console.log("Pause scene button clicked");
+
   socket.emit("adminAction", {
     sessionId: "session123",
+
     action: "pauseScene",
   });
 }
 
 //questionAction;
 
-function questionStart() {
+async function questionStart() {
   console.log("questionStart button clicked");
+
   socket.emit("adminAction", {
     sessionId: "session123",
+
     action: "questionStart",
   });
+
+  const chapterId = 1; // Example chapter ID, you might want to make this dynamic
+
+  const chapterData = await fetchChapterData(chapterId);
+
+  displayLastQuestion(chapterData);
 }
+
+function displayLastQuestion(data) {
+  if (data) {
+    const { chapter, choices } = data;
+
+    let choicesHtml = choices
+
+      .map((choice) => `<p>${choice.id}: ${choice.text}</p>`)
+
+      .join("");
+
+    const formattedText = `
+
+      <h4>${chapter}</h4>
+
+      ${choicesHtml}
+
+    `;
+
+    document.getElementById("last-question-txtbox").innerHTML = formattedText;
+  } else {
+    document.getElementById("last-question-txtbox").innerHTML =
+      "No data available.";
+  }
+}
+
 //=============================================================================================================
+
 //------------------------------API FUNCTIONS -------------------------------
 
 fetch("http://localhost:3000/api/storyAPI")
@@ -72,43 +132,74 @@ fetch("http://localhost:3000/api/storyAPI")
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
+
     return response.json();
   })
+
   .then((data) => {
     console.log(data);
   })
+
   .catch((error) => {
     console.error("There was a problem with the fetch operation:", error);
   });
+
+//--------------------------fecth Chapter --------------------------------
+
+async function fetchChapterData(chapterId) {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/storyAPI/${chapterId}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("There was a problem with the fetch operation:", error);
+  }
+}
+
 //--------------------------SOCKET --------------------------------
 
 // Emit a joinSession event upon connection
+
 socket.emit("joinSession", "session123");
 
 document.addEventListener("DOMContentLoaded", (event) => {
   document
-    .querySelector("#show-on-big-screen-btn")
-    .addEventListener("click", showPin);
 
-  //Start game
+    .querySelector("#show-on-big-screen-btn")
+
+    .addEventListener("click", showPin); //Start game
+
   document.getElementById("start-btn").onclick = () => {
     socket.emit("adminAction", {
       sessionId: "session123",
+
       action: "startGame",
     });
+
     updateEventMessage("Spillet har startet");
-  };
-  //End game
+  }; //End game
+
   document.getElementById("end-btn").onclick = () => {
     socket.emit("adminAction", {
       sessionId: "session123",
+
       action: "endGame",
     });
+
     updateEventMessage("Spillet har sluttet");
   };
 });
 
 // Listen for adminAction events from the server
+
 socket.on("adminAction", (action) => {
   if (action === "showPin") {
     console.log("Show Pin Code here");
